4c39e7ad1d78b8eb18792e70a4c206f3b110cdc4
shintakino <joshuajaynarvaza@gmail.com>
2025-03-19 03:05:37 +0800

    Partial Front End Development

--- client/src/app/(nondashboard)/landing/page.tsx
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import HeroSection from "./HeroSection";
+import FeaturesSection from "./FeaturesSection";
+import DiscoverSection from "./DiscoverSection";
+import CallToActionSection from "./CallToActionSection";
+import FooterSection from "./FooterSection";
 
 const Landing = () => {
   return (
-    <div>Landing</div>
-  )
-}
+    <div>
+      <HeroSection />
+      <FeaturesSection />
+      <DiscoverSection />
+      <CallToActionSection />
+      <FooterSection />
+    </div>
+  );
+};
 
-export default Landing
+export default Landing;
